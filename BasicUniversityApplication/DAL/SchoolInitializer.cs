cc86e25a6cf213f0c9b9b5e5b8e4e2d2122a8c18
cindy123jl <cindy123jl@gmail.com>
2021-05-28 00:19:35 -0700

    Add project files.

---
+++ BasicUniversityApplication/DAL/SchoolInitializer.cs
@@ -0,0 +1,37 @@
+﻿using BasicUniversityApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BasicUniversityApplication.DAL
+{
+    public class SchoolInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<SchoolContext>
+    {
+        protected override void Seed(SchoolContext context)
+        {
+            var students = new List<Student>
+            {
+            new Student{FirstMidName="Giorno",LastName="Giovanna",EnrollmentDate=DateTime.Parse("2020-09-01")},
+           
+            };
+
+            students.ForEach(s => context.Students.Add(s));
+            context.SaveChanges();
+            var courses = new List<Course>
+            {
+            new Course{CourseID=1050,Title="Chemistry",Credits=3,},
+          
+            };
+            courses.ForEach(s => context.Courses.Add(s));
+            context.SaveChanges();
+            var enrollments = new List<Enrollment>
+            {
+            new Enrollment{StudentID=1,CourseID=1050,Grade=Grade.A},
+   
+            };
+            enrollments.ForEach(s => context.Enrollments.Add(s));
+            context.SaveChanges();
+        }
+    }
+}
